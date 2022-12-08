5acb6517e138e4bb1ed9a3db1c574876616afcd4
JBLardet <jeanbaptiste.lardet2022@campus-eni.fr>
2022-12-08 10:08:14 +0100

    Day 6

---
+++ 6. Scroll Animation/script.js
@@ -0,0 +1,19 @@
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+    const trigger = window.innerHeight / 1.5
+
+    boxes.forEach (box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < trigger) {
+            box.classList.add('show') 
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
